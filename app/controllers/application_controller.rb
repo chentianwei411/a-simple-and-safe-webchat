7639bc2a775177351ee557337827dbb2ace53d07
ctw <ctw.3@163.com>
2018-07-19 19:14:31 +0800

    add Devise views and amend the registrations and repair the application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  # 客制化view时，devise传入参数保护。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  # skip_before_action :verify_authenticity_token
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end

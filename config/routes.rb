acb478bef3e33c33782b312d91bcf1679556404e
ctw <ctw.3@163.com>
2018-07-19 18:09:08 +0800

    View:add navbar and root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :chatrooms
   devise_for :users
+  root "chatrooms#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

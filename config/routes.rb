47a1b493c9a1a1f5a04bdd058f4f783d0b242840
ctw <ctw.3@163.com>
2018-07-19 20:45:17 +0800

    some view decoration and add routes chat_room_users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :chatrooms
+  resources :chatrooms do
+    # 不需要涉及chatroom_users的id，用resource
+    resource :chatroom_users, only: [:create, :destroy]
+  end
+
   devise_for :users
   root "chatrooms#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

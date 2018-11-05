d12bde9598e589c674e52e1d3da5a2e58e51cb95
ctw <ctw.3@163.com>
2018-11-05 10:25:02 +0800

    debug cable

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Wechat
     config.load_defaults 5.2
     config.time_zone = 'Beijing'
     # 部署
-    config.action_cable.mount_path = '/cable'
+    config.action_cable.mount_path = '/chatrooms'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
